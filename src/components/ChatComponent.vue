b2652a0f72110a17326165779971db43f63c96f7
seventrust <eondev.inc@gmail.com>
2023-04-02 12:06:44 -0400

    :sparkles: refactorización pequeña de código

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -111,7 +111,7 @@
             </button>
             <button type="submit" @click="sendMessageHere">
               <svg
-                class="w-5 h-5 text-gray-500 origin-center transform rotate-90"
+                class="w-5 h-5 text-gray-500 origin-center transform rotate-90 transition duration-150 ease-in-out hover:text-gray-700"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
